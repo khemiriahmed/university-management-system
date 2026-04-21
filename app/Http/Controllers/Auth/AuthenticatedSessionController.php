4f7d167bd34e4d7d7f2b6eb5c79b8933ba6ad06d
khemiriahmed <khemiriahmed6@gmail.com>
2026-04-21 06:29:41 +0100

    formatting and alignment for better readability

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -14,58 +14,58 @@ class AuthenticatedSessionController extends Controller
         $credentials = $request->only('email', 'password');
 
         // Essayer admin d'abord
-    if (Auth::guard('admin')->attempt($credentials)) {
-        $user = Auth::guard('admin')->user();
-        $token = $user->createToken('auth_token',[$user->getRoleAttribute()])->plainTextToken;
-        return response()->json([
-            'token' => $token,
-            'role' => 'admin'
-        ]);
-    }
+        if (Auth::guard('admin')->attempt($credentials)) {
+            $user = Auth::guard('admin')->user();
+            $token = $user->createToken('auth_token', [$user->getRoleAttribute()])->plainTextToken;
+            return response()->json([
+                'token' => $token,
+                'role' => 'admin'
+            ]);
+        }
 
-    // Essayer user (student)
-    if (Auth::guard('web')->attempt($credentials)) {
-        $user = Auth::guard('web')->user();
-        $token = $user->createToken('auth_token',[$user->getRoleAttribute()])->plainTextToken;
-        return response()->json([
-            'token' => $token,
-            'role' => 'student'
-        ]);
-    }
+        // Essayer user (student)
+        if (Auth::guard('web')->attempt($credentials)) {
+            $user = Auth::guard('web')->user();
+            $token = $user->createToken('auth_token', [$user->getRoleAttribute()])->plainTextToken;
+            return response()->json([
+                'token' => $token,
+                'role' => 'student'
+            ]);
+        }
 
 
         // Essayer user (student)
-    if (Auth::guard('teacher')->attempt($credentials)) {
-        $user = Auth::guard('teacher')->user();
-        $token = $user->createToken('auth_token',[$user->getRoleAttribute()])->plainTextToken;
+        if (Auth::guard('teacher')->attempt($credentials)) {
+            $user = Auth::guard('teacher')->user();
+            $token = $user->createToken('auth_token', [$user->getRoleAttribute()])->plainTextToken;
+            return response()->json([
+                'token' => $token,
+                'role' => 'student'
+            ]);
+        }
+
+        // Essayer user (parent)
+        if (Auth::guard('parent')->attempt($credentials)) {
+            $user = Auth::guard('parent')->user();
+            $token = $user->createToken('auth_token', [$user->getRoleAttribute()])->plainTextToken;
+            return response()->json([
+                'token' => $token,
+                'role' => 'parent'
+            ]);
+        }
+
         return response()->json([
-            'token' => $token,
-            'role' => 'student'
-        ]);
-    }
-
-           // Essayer user (parent)
-    if (Auth::guard('parent')->attempt($credentials)) {
-        $user = Auth::guard('parent')->user();
-        $token = $user->createToken('auth_token',[$user->getRoleAttribute()])->plainTextToken;
-        return response()->json([
-            'token' => $token,
-            'role' => 'parent'
-        ]);
-    }
-
-     return response()->json([
-        'message' => 'Invalid credentials'
-    ], 401);
+            'message' => 'Invalid credentials'
+        ], 401);
 
 
-       
+
     }
 
     public function destroy(Request $request)
     {
         $request->user()->currentAccessToken()->delete();
-       
+
 
         return response()->json([
             'message' => 'Logged out successfully'
